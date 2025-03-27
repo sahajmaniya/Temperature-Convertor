a36cbd830ca9b0a2067b4c1aec6a09060e343496
sahajmaniya <sahajmaniya1120@gmail.com>
2025-03-27 15:04:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function convertCelsiusToFahrenheit() {
+  var celsiusInput = document.getElementById("celsius");
+  var fahrenheitInput = document.getElementById("fahrenheit");
+
+  var celsius = parseFloat(celsiusInput.value);
+  var fahrenheit = (celsius * 9) / 5 + 32;
+
+  fahrenheitInput.value = fahrenheit;
+}
+
+function convertFahrenheitToCelsius() {
+  var celsiusInput = document.getElementById("celsius");
+  var fahrenheitInput = document.getElementById("fahrenheit");
+
+  var fahrenheit = parseFloat(fahrenheitInput.value);
+  var celsius = ((fahrenheit - 32) * 5) / 9;
+
+  celsiusInput.value = celsius;
+}
